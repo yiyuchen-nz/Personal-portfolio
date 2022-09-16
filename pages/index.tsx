7c7006abea1b384de2ea9d183fe472f54fad7c5d
yiyuchen-nz <yiyuchen9@gmail.com>
2022-09-16 23:10:08 +1200

    created api and worked on about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
+//import { GetServerSidePropsContext } from 'next'; 
+import ServiceCard from '../components/ServiceCard';
 import {services}from'../data'
 
-const index = () => {
+const index = () => { 
+  
   return(
-    <div>
-        <h1 className="">Hello!</h1>
+    <div className='flex flex-col flex-grow px-6 pt-1'>
+        <h5 className='my-3 font-medium'>Hi! This is all about me.Hi! This is all about me.Hi! This is all about me.Hi! This is all about me.Hi! This is all about me.Hi! This is all about me.Hi! This is all about me.</h5>
+        <div className='flex-grow p-4 mt-5 bg-gray-400' style={{marginLeft:'-1.5rem', marginRight:'-1.5rem'}}>
+          <h6 className='my-3 text-xl font-bold tracking-wide'>What I offer</h6>
+          <div className='grid gap-6 lg:grid-cols-2'>
+            {services.map((service,idx)=>(
+              <div className='bg-gray-200 rounded-lg lg:col-span-1' key={idx}>
+            <ServiceCard service={service}/>
+            </div>
+            ))}
+          </div>
+        </div>
     </div>
-  )
+  ) 
 }
-export default index
+export default index  
+
+// export const getServerSideProps =async (context: GetServerSidePropsContext)=>{
+
+//   const res = await fetch('http://localhost:3000/api/services')
+//   const data = await res.json()
+
+  
+//   return{
+//     props:{
+//       services: data.services
+//     }
+//   }
+// } 
