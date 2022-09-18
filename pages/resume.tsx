c4834ba402dded1ff8ea438735835ba3905be5c6
yiyuchen-nz <yiyuchen9@gmail.com>
2022-09-18 21:44:16 +1200

    updated Readme

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,6 +1,35 @@
 const resume = () => {
   return (
-    <div className="p-4"> resume page</div>
+    <div className="px-6 py-2"> 
+    
+    {/* //education and experience */}
+    <div className="grid gap-6 md:grid-cols-2">
+      <div>
+        <h5 className="my-3 text-2xl font-bold">Education</h5>
+        <div>
+          <h5 className="my-2 text-xl font-bold ">
+            boot camp in EDA
+          </h5>
+          <p className="font-semibold">year(April 2022- July 2022)</p>
+          <p className="my-3">What i am doing. What i am doing. What i am doing. What i am doing. What i am doing. What i am doing. What i am doing.</p>
+        </div>
+      </div>
+    </div>
+
+    {/* languages and tools */}
+    <div className="grid gap-6 md:grid-cols-2">
+      <div>
+        <h5 className="my-3 text-2xl font-bold">Education</h5>
+        <div>
+          <h5 className="my-2 text-xl font-bold ">
+            boot camp in EDA
+          </h5>
+          <p className="font-semibold">year(April 2022- July 2022)</p>
+          <p className="my-3">What i am doing. What i am doing. What i am doing. What i am doing. What i am doing. What i am doing. What i am doing.</p>
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
