64b022fda04caa15c9c8cc853271e77176b4f8b3
yiyuchen-nz <yiyuchen9@gmail.com>
2022-09-21 12:04:08 +1200

    project navbar style

--- components/ProjectsNavBar.tsx
+++ components/ProjectsNavBar.tsx
@@ -1,16 +1,19 @@
 import { FunctionComponent } from "react"
 import { Category } from "../type"
 
-export const NavItem:FunctionComponent<{value:Category | "all", handlerFilterCategory:Function}> = ({value,
-  handlerFilterCategory
+export const NavItem:FunctionComponent<{value:Category | "all", handlerFilterCategory:Function, active:string}> = ({value,
+  handlerFilterCategory,active
 }) => {
+  let className= "capitalize cursor-pointer hover:text-blue"
+  if(active === value) className += " text-blue"
+
   return (
-   <li className="cursor-pointer hover:text-darkBlue" onClick={()=>{handlerFilterCategory(value)}}>{value}</li>
+   <li className={className} onClick={()=>{handlerFilterCategory(value)}}>{value}</li>
   )
 }
 
 
-const ProjectsNavBar:FunctionComponent<{handlerFilterCategory:Function}> = (props) => {
+const ProjectsNavBar:FunctionComponent<{handlerFilterCategory:Function, active:string}> = (props) => {
   return (
     <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
       <NavItem value="all" {...props}/>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -23,7 +23,7 @@ const Projects = () => {
 
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{height:"65vh"}}>
-      <ProjectsNavBar handlerFilterCategory={handleFilterCategory}/>
+      <ProjectsNavBar handlerFilterCategory={handleFilterCategory} active={active}/>
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map(project=>(
           <div  key={project.name} className="col-span-12 p-2 bg-gray-200 rounded-sm sm:col-span-6 lg:col-span-4 dark:bg-black">
